ac7b5f95067e838c893ec7351030c9e6d1043993
YuchinP <yuchinspan@gmail.com>
2017-05-26 22:46:38 -0700

    switched to sqli and consolidated method

--- sqlConnector.php
+++ sqlConnector.php
@@ -1,30 +1,33 @@
 <?php
 
-class pList {
-  public $name;
-}
-
 //Get Values from login page
 //$user = $_POST['username'];
 
 //Connect to the database
-  $connection = mysqli_connect("localhost", "root", "", "draftdb");
+$connection = mysqli_connect("localhost", "root", "", "draftdb");
 
-  //Option 1 Vardump correspondes to the pList class
-  $pokemonList = mysql_query("SELECT forme FROM pokemon limit 1");
-  $obj = mysql_fetch_object($result, 'pList');
-  var_dump($obj);
+//Check connection
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
 
+//Query
+$pokemonList = mysqli_query("SELECT forme FROM pokemon limit 1");
 
-  // Side option if the class var_dump doesn't work
-  // Query the full database
-  // $pokemonList = mysqli_query($connection, "SELECT forme FROM pokemon")
-  //  or die("Failed to query database ".mysql_error());
-  //
-  // while ($row = mysql_fetch_object($pokemonList)) {
-  //  echo $row->forme;
-  // }
-  // mysql_free_result($pokemonList)
+//loop the data
+if ( ($result = mysqli_query($connection, $pokemonList)) {
 
+  //fetch associative array
+  while ($row = mysql_fetch_assoc($result)) {
+    printf ("%s\n", $row["forme"]);
+  }
+
+  //free result set
+  mysqli_free_result($result)
+}
+
+//close connection
+mysqli_close($connection);
 
 ?>
